6f74a1123c0e4b1174ac76277d52422e86144ecf
Carlos Quinto <carlquin77@gmail.com>
2019-06-18 04:04:02 -0700

    added functionality to initial generation of react components, added images for components, added json for image organization and reference.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CardBody from "./components/card/CardBody";
+import cards from "./cards.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.cards to the array of cards
+  state = {
+    cards,
+    scoreCurr: 0,
+    scoreTop: 0,
+    gameOver: false
+  };
+
+  // Randomize location of cards
+  // handleShuffle = () => {
+
+  // }
+
+  // Increase score count by 1
+  handleWin = () => {
+    // Manage score, compare to top score
+    this.setState({ scoreCurr: this.state.scoreCurr + 1 });
+
+    if (this.state.scoreCurr > this.state.scoreTop) this.setState({ scoreCurr: this.state.scoreCurr });
+
+    // Shuffle the game space
+    // this.handleShuffle();
+    return true;
+  };
+
+  // Increase score count by 1
+  handleLoss = () => {
+    this.setState({ gameOver: true });
+    console.log("Game Over");
+    return false;
+  };
+
+  handleSelect = () => {
+    this.setState({ chosen: this.state.chosen ? this.state.handleLoss : this.state.handleWin })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Click Memory Game</h1>
+        </header>
+        <div className="wrapper">
+          <div className="over-wrap">
+            {this.state.cards.map(card => (
+              <CardBody
+                key={this.state.cards.indexOf(card)}
+                datasource={card.datasource}
+                name={card.name}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
